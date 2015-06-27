c52cb98b418c862c0d125b8806877202863e9138
blumak2000 <blumalorom@gmail.com>
2015-06-27 21:59:25 +0200

    added alucard,nightmare,ondemandplus governor,set nightmare as default

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -413,6 +413,9 @@ extern struct cpufreq_governor cpufreq_gov_bluactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
 extern struct cpufreq_governor cpufreq_gov_nightmare;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nightmare)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
+extern struct cpufreq_governor cpufreq_gov_ondemandplus;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandplus)
 #endif
 
 
